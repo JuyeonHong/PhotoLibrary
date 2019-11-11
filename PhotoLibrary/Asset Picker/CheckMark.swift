243faaada31e043dd1783ade79fe76b920e7bf49
hongjuyeon <coke1317@naver.com>
2019-11-11 15:41:22 +0900

    CollectionView - CheckMark class 추가

---
+++ PhotoLibrary/Asset Picker/CheckMark.swift
@@ -0,0 +1,22 @@
+//
+//  CheckMark.swift
+//  PhotoLibrary
+//
+//  Created by hongjuyeon_dev on 2019/11/11.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import UIKit
+import CoreGraphics
+
+class CheckMark: UIView{
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        // draw check
+    }
+    
+    func drawRectChecked() {
+        // General Declaration
+        let context = UIGraphicsGetCurrentContext()
+    }
+}
