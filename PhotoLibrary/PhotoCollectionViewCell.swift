74bbab584b79eb43f7d995235a5028295960b0ab
hongjuyeon <coke1317@naver.com>
2019-11-14 16:31:04 +0900

    PhotoCollectionViewCell image 받는 부분 추가

--- PhotoLibrary/PhotoCollectionViewCell.swift
+++ PhotoLibrary/PhotoCollectionViewCell.swift
@@ -9,5 +9,17 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    var representedAssetIdentifier: String?
+    var thumbnailImage: UIImage? {
+        didSet {
+            imageView.image = thumbnailImage
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
     
 }
